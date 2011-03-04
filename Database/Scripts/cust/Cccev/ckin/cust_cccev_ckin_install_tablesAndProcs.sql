83f66e4982eb739e9d4e561a136c858aa49a0a7d
Daniel Hazelbaker <cabal95@users.noreply.github.com>
2011-03-04 17:41:28 +0000

    Added the cust_cccev_ckin_sp_get_location_head_count_by_date sproc to the install SQL script.

--- Database/Scripts/cust/Cccev/ckin/cust_cccev_ckin_install_tablesAndProcs.sql
+++ Database/Scripts/cust/Cccev/ckin/cust_cccev_ckin_install_tablesAndProcs.sql
@@ -516,6 +516,7 @@ GO
 
 --------------------------------------------------------------------------------
 -- v1.3.0 Add two functions that are used by the sample RS label reports -------
+--        Add one stored procedure that should have already existed      -------
 --------------------------------------------------------------------------------
 
 -- #1 cust_hdc_funct_calc_age
@@ -576,6 +577,29 @@ BEGIN
 
 	RETURN @ParentNames
 END
+GO
+
+-- #3 cust_cccev_ckin_sp_get_location_head_count_by_date
+--------------------------------------------------------
+if exists (select * from dbo.sysobjects where id = object_id(N'[cust_cccev_ckin_sp_get_location_head_count_by_date]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
+	drop procedure [cust_cccev_ckin_sp_get_location_head_count_by_date]
+GO
+
+CREATE Proc [dbo].[cust_cccev_ckin_sp_get_location_head_count_by_date]
+@LocationID int,
+@StartDate datetime
+AS
+
+	SELECT COUNT(distinct oa.person_id)
+	FROM core_occurrence O
+	INNER JOIN core_occurrence_attendance OA ON O.occurrence_id = OA.occurrence_id
+	WHERE O.location_id = @LocationID
+	AND (O.occurrence_start_time <= @StartDate OR check_in_start <= @StartDate)
+	AND (O.occurrence_end_time >= @StartDate OR check_in_end >= @StartDate)
+	AND OA.attended = 1
+	AND OA.check_in_time IS NOT NULL
+	AND OA.check_in_time <> '1/1/1900'
+	AND OA.check_out_time = '1/1/1900'
 GO
 
 --------------------------------------------------------------------------------
